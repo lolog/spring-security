cda7c982a167452bcc672df333ef4631d63b1f79
lolog <jonguo@yeah.net>
2017-11-14 01:33:11 +0800

    v=4.2 and simple and login-page

---
+++ security-login-page/src/main/java/net/spring/security/handler/AuthSuccessHandler.java
@@ -0,0 +1,18 @@
+package net.spring.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth)
+            throws IOException, ServletException {
+
+        System.out.println("[info] net.spring.security.handler.AuthSuccessHandler");
+        response.sendRedirect(request.getContextPath());
+    }
+}
---
+++ security-login-page/src/main/webapp/auth/index.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Security Auth</title>
+</head>
+<body>
+    <h1>Security Authority Successful...</h1>
+</body>
+</html>
